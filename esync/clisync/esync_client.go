82c5928cb7a15e7319db3c8e3b869edd648108dc
Damien Stamates <thedoubledimensions@gmail.com>
2025-06-25 08:45:28 -0400

    feat: client-side interpolation (#5)
    * update dependencies
    
    * remove unused code
    
    * more unused code
    
    * rough outline of client-side interpolation
    
    * small bits of cleanup
    
    * reduce checks
    
    * comments on added exported functions
    
    * use uint8 for interp ids and add custom binary marshalers
    
    * change historic map to array
    
    * integrate cleaner with current api
    
    * update comments for clarity
    
    * change id to component map to array

--- esync/clisync/esync_client.go
+++ esync/clisync/esync_client.go
@@ -2,11 +2,15 @@ package clisync
 
 import (
 	"fmt"
+	"reflect"
+	"time"
+
 	"github.com/leap-fish/necs/esync"
 	"github.com/leap-fish/necs/router"
 	"github.com/yohamta/donburi"
-	"reflect"
 )
+
+const MaxHistorySize = 32
 
 func clientUpdateWorldState(world donburi.World, state esync.WorldSnapshot) error {
 	for _, ent := range state {
@@ -26,16 +30,19 @@ func clientUpdateWorldState(world donburi.World, state esync.WorldSnapshot) erro
 }
 
 func applyEntityDiff(world donburi.World, networkId esync.NetworkId, components []any) {
-	var ctypes = make([]donburi.IComponentType, 0)
+	ctypes := make([]donburi.IComponentType, 0)
+	refTypes := make([]reflect.Type, len(components))
 
-	for _, componentData := range components {
+	for i, componentData := range components {
 		componentType := reflect.TypeOf(componentData)
 		ctype, ok := esync.Registered(componentType)
 		if !ok {
 			// TODO: Add back erroring here
 			continue
 		}
+
 		ctypes = append(ctypes, ctype)
+		refTypes[i] = componentType
 	}
 
 	entity := esync.FindByNetworkId(world, networkId)
@@ -45,14 +52,50 @@ func applyEntityDiff(world donburi.World, networkId esync.NetworkId, components 
 	}
 
 	entry = world.Entry(entity)
+	now := time.Now()
+
+	// calculate average latency and the delay index
+	calculateDelay(now)
 
 	if entry != nil && world.Valid(entity) {
+		interpolated := entry.HasComponent(esync.InterpComponent)
+
 		for i := 0; i < len(components); i++ {
 			data := components[i]
 			if data == nil {
 				panic("meow")
 			}
-			entry.SetComponent(ctypes[i], esync.ComponentFromVal(ctypes[i], data))
+
+			ok := esync.RegisteredInterpType(refTypes[i])
+			if !ok || !interpolated {
+				entry.SetComponent(ctypes[i], esync.ComponentFromVal(ctypes[i], data))
+				continue
+			}
+
+			key := esync.LookupInterpId(refTypes[i])
+			// Add the base value for this component if it doesn't have one
+			if !entry.HasComponent(ctypes[i]) {
+				entry.SetComponent(ctypes[i], ctypes[i].New())
+			}
+
+			// Add a component cache to keep track of historic values for this
+			// interpolated component
+			if !entry.HasComponent(timeCacheComponent) {
+				donburi.Add(entry, timeCacheComponent, &timeCacheData{})
+			}
+
+			// Append the new value to our historic cache with its associated
+			// timestamp of when we received this
+			multHistory := timeCacheComponent.Get(entry)
+			multHistory.history[key] = append(multHistory.history[key], componentTimeData{
+				value: data,
+				ts:    now,
+			})
+
+			// Shift the positions if we've reached the limit
+			if len(multHistory.history[key]) > MaxHistorySize {
+				multHistory.history[key] = multHistory.history[key][1:]
+			}
 		}
 	}
 }
@@ -61,6 +104,7 @@ func RegisterClient(world donburi.World) {
 	router.On[esync.WorldSnapshot](func(sender *router.NetworkClient, message esync.WorldSnapshot) {
 		err := clientUpdateWorldState(world, message)
 		if err != nil {
+			panic(err)
 			// TODO: Add back error handling here
 		}
 
--- esync/esync_shared.go
+++ esync/esync_shared.go
@@ -1,12 +1,63 @@
 package esync
 
 import (
+	"bytes"
+	"encoding/binary"
+	"reflect"
+	"unsafe"
+
 	"github.com/leap-fish/necs/typemapper"
 	"github.com/yohamta/donburi"
 	"github.com/yohamta/donburi/filter"
-	"reflect"
-	"unsafe"
 )
+
+var InterpComponent = donburi.NewComponentType[InterpData]()
+
+type InterpData struct {
+	Components []uint8
+}
+
+func (id *InterpData) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+	buf.Grow(len(id.Components))
+
+	for i := range id.Components {
+		binary.Write(&buf, binary.LittleEndian, id.Components[i])
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (id *InterpData) UnmarshalBinary(data []byte) error {
+	buf := bytes.NewReader(data)
+
+	id.Components = make([]uint8, buf.Len())
+	for i := 0; i < len(id.Components); i++ {
+		binary.Read(buf, binary.LittleEndian, &id.Components[i])
+	}
+
+	return nil
+}
+
+func NewInterpData(components ...donburi.IComponentType) *InterpData {
+	ids := []uint8{}
+	for i := range components {
+		key := interpolated.LookupId(components[i].Typ())
+		if key == 0 {
+			continue
+		}
+
+		ids = append(ids, key)
+	}
+
+	return &InterpData{
+		Components: ids,
+	}
+}
+
+func (i *InterpData) ComponentKeys() []uint8 {
+	return i.Components
+}
 
 type ComponentId uint
 type NetworkId uint
@@ -18,8 +69,12 @@ type SerializedEntity struct {
 }
 type WorldSnapshot []SerializedEntity
 
+// LerpFn is used by the InterpolateSystem to properly lerp your component
+type LerpFn[T any] func(from T, to T, delta float64) *T
+
 var NetworkEntityQuery = donburi.NewQuery(filter.Contains(NetworkIdComponent))
 
+var interpolated = typemapper.NewComponentMapper()
 var registered = map[reflect.Type]donburi.IComponentType{}
 
 var (
@@ -27,20 +82,88 @@ var (
 	NetworkIdComponent = donburi.NewComponentType[NetworkId]()
 )
 
+// LookInterpId returns the interpolation ID for the given type, if not present
+// then 0 is returned.
+func LookupInterpId(typ reflect.Type) uint8 {
+	return interpolated.LookupId(typ)
+}
+
+// LookupInterpType returns the component type for the given interpolation ID,
+// if not present then an empty reflect.Type is returned.
+func LookupInterpType(id uint8) reflect.Type {
+	return interpolated.LookupType(id)
+}
+
+// LookupInterpSetter returns the setter function for the given interpolation ID.
+// This should always be type [esync.LerpFn]
+func LookupInterpSetter(id uint8) reflect.Value {
+	return interpolated.LookupSetter(id)
+}
+
+// RegisteredInterpId returns true if the given interpolation ID is registered.
+func RegisteredInterpId(id uint8) bool {
+	return interpolated.RegisteredId(id)
+}
+
+// RegisteredInterpType returns true if the given component type is registered for
+// interpolation.
+func RegisteredInterpType(typ reflect.Type) bool {
+	return interpolated.RegisteredType(typ)
+}
+
 func Registered(componentType reflect.Type) (donburi.IComponentType, bool) {
 	ctype, ok := registered[componentType]
 	return ctype, ok
 }
 
+type RegisterOption[T any] func(*donburi.ComponentType[T])
+
+// WithInterpFn will utilize the given lerp function for client-side interpolation
+// when registering with a component.
+//
+// For example you can provide the following for a basic Position Component:
+//
+//	var PositionComponent = donburi.NewComponentType[Vector2]()
+//
+//	type Vector2 struct {
+//		X, Y float64
+//	}
+//
+//	func lerp(a, b, t float64) float64 {
+//		return (1.0-t)*a + b*t
+//	}
+//
+//	func lerpVec2(from, to Vector2, delta float64) *Vector2 {
+//		return &Vector2{
+//			X: lerp(from.X, to.X, delta),
+//			T: lerp(from.Y, to.Y, delta),
+//		}
+//	}
+//
+//	esync.RegisterComponent(10, Vector2{}, PositionComponent, esync.WithInterpFn(10, lerpVec2))
+func WithInterpFn[T any](id uint8, fn LerpFn[T]) RegisterOption[T] {
+	return func(ctype *donburi.ComponentType[T]) {
+		interpolated.RegisterInterpolatedComponent(id, ctype, fn)
+	}
+}
+
 // RegisterComponent registers a component for use with esync. Make sure the client and server have the same definition of components.
 // Note that ID 1 is reserved for the NetworkId component used by esync.
-func RegisterComponent(id uint, component any, ctype donburi.IComponentType) error {
+//
+// Optionally you may provide an optional [WithInterpFn] to register this component
+// for interpolation.
+func RegisterComponent[T any](id uint, component any, ctype *donburi.ComponentType[T], opt ...RegisterOption[T]) error {
 	typ := reflect.TypeOf(component)
 	err := Mapper.RegisterType(id, typ)
 	if err != nil {
 		return err
 	}
 	registered[typ] = ctype
+
+	// Call the options
+	for _, o := range opt {
+		o(ctype)
+	}
 
 	return nil
 }
